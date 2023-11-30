e0bc7181600ad17551bc7c887a18dd2803f38215
SaracenRhue <77064702+SaracenRhue@users.noreply.github.com>
2023-11-30 18:22:42 +0100

    Update transcode.sh

--- transcode.sh
+++ transcode.sh
@@ -7,4 +7,4 @@ if [ -z "$INPUT_DIR" ] || [ -z "$OUTPUT_DIR" ]; then
 fi
 
 # Transcoding command using FFmpeg and GNU Parallel
-parallel --eta -j 75% ffmpeg -i {} -c:v libaom-av1 -crf 18 -cpu-used 0 -pass 1 -f null /dev/null \; ffmpeg -i {} -c:v libaom-av1 -crf 18 -cpu-used 0 -pass 2 $OUTPUT_DIR/{/.}.mkv ::: $INPUT_DIR/*.mp4
+parallel --eta -j 75% ffmpeg -i {} -c:v libaom-av1 -crf 18 -cpu-used 0 -pass 1 -f null /dev/null \; ffmpeg -i {} -c:v libaom-av1 -crf 18 -cpu-used 0 -pass 2 $OUTPUT_DIR/{/.}.mkv ::: $INPUT_DIR/*.*
